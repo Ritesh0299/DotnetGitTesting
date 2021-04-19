91897f83a10643d3cdb96086b6ac0e912306d05b
Ritesh Moolya <riteshmoolya02@gmail.com>
2021-04-19 17:16:54 +0530

    Hello Mind

---
+++ FirstAppDemo/FirstAppDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstAppDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Mindtree");
+            Console.WriteLine("Hello Hello India");
+        }
+    }
+}
